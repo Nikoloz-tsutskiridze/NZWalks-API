d5c38f89c344a344ebbca3a57de6e4239528b9a1
Nikoloz Tsutskiridze <nikacucqiridze2000@gmail.com>
2025-03-11 00:23:49 +0400

    Users Auth

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerId = "c66a054a-19a2-464b-b209-f5acd81ee1a8";
+            var writerId = "2dd932e7-8ccb-4beb-ac63-dc14d6af1947";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                 Id = readerId,
+                 ConcurrencyStamp = readerId,
+                 Name="Reader",
+                 NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                 Id = writerId,
+                 ConcurrencyStamp = writerId,
+                 Name="Writer",
+                 NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
---
+++ NZWalks.API/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
